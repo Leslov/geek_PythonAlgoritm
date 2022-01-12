0acaf51d7c9cad7a56b80b64fb6c9829cb2537b8
Pavlov Aleksey <lehaha95@mail.ru>
2022-01-12 11:10:41 +0500

    HW6

--- hw6/ex1.py
+++ hw6/ex1.py
@@ -3,7 +3,9 @@
 
 # Сумма ряда чисел 1, -0.5, 0.25, -0.125,…
 import sys
-from pympler import asizeof
+
+
+# from pympler import asizeof
 
 def test_func(func):
     results = [
@@ -32,85 +34,108 @@ def num_func_1(n):
     for i in range(n):
         summ += nth_elem
         nth_elem /= -2
+    memory_check(f'num_func_1({n})', nth_elem, summ)
     return summ
 
 
-def num_func_2(N):
-    def calc_top(n):
+def num_func_2(n):
+    def calc_top(num):
         summ = 0
-        for i in range(n):
+        for i in range(num):
             summ |= 1 << i * 2
         return summ
 
-    nm = N // 2  # число операций для поиска отрицательной суммы
-    np = N - nm  # то же, для положительной суммы
+    nm = n // 2  # число операций для поиска отрицательной суммы
+    np = n - nm  # то же, для положительной суммы
     dif = np - nm
     mt = calc_top(nm)
     mb = 4 ** (nm - 1) * 2
-    pt = mt + dif * (1 << (nm*2))
-    pb = 1 << ((np - 1)*2)
+    pt = mt + dif * (1 << (nm * 2))
+    pb = 1 << ((np - 1) * 2)
+    memory_check(f'num_func_2({n})', nm, np, dif, mt, mb, pt, pb)
     return pt / pb - mt / mb
 
 
-def num_func_3(N):
-    def calc_top(n):
-        if n == 0:
+def num_func_3(n):
+    def calc_top(num):
+        if num == 0:
             return 0
-        c = f'0b{"01" * n}' # Битовая маска
-        return int(c, 2) # На выходе получается для n = 1, 2, 3...: 1, 5, 21, 85...
-    nm = N // 2  # число операций для поиска отрицательной суммы
-    np = N - nm  # то же, для положительной суммы
+        c = f'0b{"01" * num}'  # Битовая маска
+        return int(c, 2)  # На выходе получается для n = 1, 2, 3...: 1, 5, 21, 85...
+
+    nm = n // 2  # число операций для поиска отрицательной суммы
+    np = n - nm  # то же, для положительной суммы
     dif = np - nm
     mt = calc_top(nm)
     mb = 4 ** (nm - 1) * 2
-    pt = mt + dif * (1 << (nm*2))
-    pb = 1 << ((np - 1)*2) # 4 ** (np - 1)
-    print(locals())
-    #memory_check(locals())
-
+    pt = mt + dif * (1 << (nm * 2))
+    pb = 1 << ((np - 1) * 2)  # 4 ** (np - 1)
+    memory_check(f'num_func_3({n})', nm, np, dif, mt, mb, pt, pb)
     return pt / pb - mt / mb
+
 
 def test(func, rates):
-    #test_func(func)
+    # test_func(func)
 
     for rate in rates:
         func(rate)
-        #res = sys.getsizeof(1)#asizeof.asizeof(1)
-        #res = timeit(lambda: func(rate), number=1000)
-        #print(f'{func.__name__}({rate}): {res}')
+        # res = sys.getsizeof(1)#asizeof.asizeof(1)
+        # res = timeit(lambda: func(rate), number=1000)
+        # print(f'{func.__name__}({rate}): {res}')
 
-def memory_check(*variables):
+
+def memory_check(func_name, *variables):
     memory_size = 0
-    print(variables)
+    # print(variables)
     for var in variables:
         memory_size += sys.getsizeof(var)
-    print(memory_size)
+    print(f'{func_name} uses {memory_size} bytes of memory')
     return memory_size
 
 
-rates = [100, 1000, 10000, 100000]
-funcs = [num_func_1,num_func_2,num_func_3]
-#for func in funcs:
+rates = [100, 1000, 10000, 100000, 1000000]
+funcs = [num_func_1, num_func_2, num_func_3]
+# for func in funcs:
 #    test(func, rates)
 
+test(num_func_1, rates)
+test(num_func_2, rates)
 test(num_func_3, rates)
 
-# Детали: Время выполнения - примерное, актуально только для текущего состояния моего компа
-# func 'num_func_1' is OK
+# Детали:
+# num_func_1:
+# Объем занимаемой памяти:
+# num_func_1(100)       uses 48 bytes of memory
+# num_func_1(1000)      uses 48 bytes of memory
+# num_func_1(10000)     uses 48 bytes of memory
+# num_func_1(100000)    uses 48 bytes of memory
+# Время выполнения:
 # num_func_1(100):      0.004996
 # num_func_1(1000):     0.051549
 # num_func_1(10000):    0.531762
 # num_func_1(100000):   5.206050
 # Сложность линейная. Время выполнения на последней стадии (сек) t = n / 20000
 
-# func 'num_func_2' is OK
+# num_func_2:
+# Объем занимаемой памяти:
+# num_func_2(100)       uses 240 bytes of memory
+# num_func_2(1000)      uses 720 bytes of memory
+# num_func_2(10000)     uses 5520 bytes of memory
+# num_func_2(100000)    uses 53520 bytes of memory
+# Время выполнения:
 # num_func_2(100):      0.004536
 # num_func_2(1000):     0.045906
 # num_func_2(10000):    3.244698
 # num_func_2(100000):   113.765463
 # Сложность нелинейная. Время выполнения на последней стадии (сек) t = n / 885
 
-# func 'num_func_3' is OK
+# num_func_3:
+# Объем занимаемой памяти:
+# num_func_3(100)       uses 240 bytes of memory
+# num_func_3(1000)      uses 720 bytes of memory
+# num_func_3(10000)     uses 5520 bytes of memory
+# num_func_3(100000)    uses 53520 bytes of memory
+# Время выполнения:
 # num_func_3(100):      0.001436
 # num_func_3(1000):     0.007611
 # num_func_3(10000):    0.042845
@@ -118,5 +143,8 @@ test(num_func_3, rates)
 # num_func_3(1000000):  7.018619
 # Сложность почти линейная :). Время выполнения (сек) t = n / 142000
 
-# Оптимальный алгоритм - №3. Его можно и дальше проапгрейдить.
+# Оптимальный алгоритм по объему занимаемой памяти - №1. Размер используемой памяти
+# Оптимальный алгоритм по времени выполнения - №3. Его можно и дальше проапгрейдить.
 # Нужно найти более оптимальный способ формирования битовой маски.
+# Худший алгоритм - №2. Он и по памяти невыгоден и по времени выполнения
+# Время выполнения растет в геометрической прогрессии. Объем занимаемой памяти растет линейно
